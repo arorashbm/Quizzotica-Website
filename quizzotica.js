23cc39a2db0e99d357a877a4218422c508cb555b
arorashbm <shubhamar@iitbhilai.ac.in>
2019-12-30 23:41:55 +0530

    loader changed

--- quizzotica.js
+++ quizzotica.js
@@ -1,7 +1,4 @@
 var loader = document.getElementsByClassName("loader")[0];
-var loader_gif = Math.floor(Math.random()*4) + 1;
-
-loader.style.background = "url('./loader/" + loader_gif + ".gif') 50% 50% no-repeat white";
 
 window.onload = function() {
     document.getElementsByTagName("body")[0].style.overflow = "auto";
